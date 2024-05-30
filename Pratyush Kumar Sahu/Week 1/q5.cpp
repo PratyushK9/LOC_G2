5e1bf7960f2f57482a1757a39efbdab513a3f171
Pratyush Kumar <139534961+PratyushK9@users.noreply.github.com>
2024-05-30 13:29:08 +0530

    Add files via upload
    weekly task completed

---
+++ Pratyush Kumar Sahu/Week 1/q5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<< "Enter the radius of the circle: ";
+    cin>> r;
+    cout <<"the diameter of the circle is "<< 2*r<< endl;
+    cout <<"the circumference of the circle is "<< 2*r*3.14<< endl;
+    cout <<"the diameter of the circle is 23"<< r*r*3.14<< endl;    
+}
